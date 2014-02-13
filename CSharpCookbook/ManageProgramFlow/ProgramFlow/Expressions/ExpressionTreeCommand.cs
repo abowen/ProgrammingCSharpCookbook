9bde023fe34658d0594340a1acab407ad7c130cb
abowen <andrew.james.bowen@gmail.com>
2014-02-13 20:54:21 +1100

    Added advanced expression tree example

--- CSharpCookbook/ManageProgramFlow/ProgramFlow/Expressions/ExpressionTreeCommand.cs
+++ CSharpCookbook/ManageProgramFlow/ProgramFlow/Expressions/ExpressionTreeCommand.cs
@@ -9,6 +9,7 @@ namespace CSharpCookbook.ManageProgramFlow.ProgramFlow.Expressions
         public ExpressionTreeCommand()
         {
             AddResource("Expression Trees", "http://msdn.microsoft.com/en-us/library/vstudio/bb397951.aspx");
+            AddResource("Expression Trees, Location 15969", "C# In A Nutshell");
         }
 
         public override string Description
@@ -19,14 +20,38 @@ namespace CSharpCookbook.ManageProgramFlow.ProgramFlow.Expressions
         delegate int MyDelegate(int i);
 
         public override void ExecuteDemo()
-        {            
-            Expression<MyDelegate> expression = x => x * x;            
-            var value = expression.Compile().Invoke(5);
-            Console.WriteLine("Using Delegate: {0}", value);            
-            
+        {
+            DelegateDemo();
+            FuncDemo();
+            HandWrittenDemo();
+        }
+
+        private void HandWrittenDemo()
+        {
+            // C# In A NutShell. Start from bottom up
+            // Expression<Func<string, bool>> f = s => s.Length < 5;
+            ParameterExpression p = Expression.Parameter(typeof(string), "s");
+            MemberExpression stringLength = Expression.Property(p, "Length");
+            ConstantExpression five = Expression.Constant(5);
+            BinaryExpression comparison = Expression.LessThan(stringLength, five);
+            Expression<Func<string, bool>> lambda = Expression.Lambda<Func<string, bool>>(comparison, p);
+            Func<string, bool> runnable = lambda.Compile();
+            Console.WriteLine(runnable("LongWord"));
+            Console.WriteLine(runnable("Short"));            
+        }
+
+        private static void FuncDemo()
+        {
             Expression<Func<int, bool>> lambda = num => num < 5;
             var isTrue = lambda.Compile()(5);
-            Console.WriteLine("Using Func: {0}", isTrue);            
+            Console.WriteLine("Using Func: {0}", isTrue);
+        }
+
+        private static void DelegateDemo()
+        {
+            Expression<MyDelegate> expression = x => x*x;
+            var value = expression.Compile().Invoke(5);
+            Console.WriteLine("Using Delegate: {0}", value);
         }
     }
 }
